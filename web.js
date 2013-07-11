32387a33582b4a351125365e9f6da62e14617113
buihaian <haian@tgm.vn>
2013-07-11 10:28:09 +0700

    assignment work

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require('express'), fs=require('fs');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
